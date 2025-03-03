c24ebaca3d81f8b6a741bc10feb138ce90cb17dd
D <dector@dector.space>
2025-03-04 00:28:25 +0100

    wip: generate non-wrapped html files for log entries

--- internal/glow/build.go
+++ internal/glow/build.go
@@ -11,20 +11,26 @@ import (
 )
 
 type RawLogItem struct {
-	file    string
-	content string
+	index      string
+	title      string
+	fileName   string
+	rawContent string
 }
+
+var DirOut = "out"
+var FileGlowMark = ".glow_build"
 
 func Build() {
 	fmt.Println("Building...")
+
+	prepareOutDir(DirOut)
+
 	items := fetchLogItems()
 
 	for _, i := range items {
-		os.Mkdir("out", os.ModePerm)
-		htmlContent := renderMarkdown(i.content)
-
-		filePath := filepath.Join("out", fmt.Sprintf("%s.html", i.file))
-		os.WriteFile(filePath, []byte(htmlContent), os.ModePerm)
+		filePath := filepath.Join(DirOut, i.index, "index.html")
+		os.MkdirAll(filepath.Dir(filePath), os.ModePerm)
+		os.WriteFile(filePath, buildHtmlFileContent(i), os.ModePerm)
 	}
 
 	// iterate over files
@@ -40,12 +46,20 @@ func fetchLogItems() []RawLogItem {
 	files, _ := os.ReadDir(dirPath)
 	for _, f := range files {
 		filePath := filepath.Join(dirPath, f.Name())
-		fileName := strings.TrimSuffix(f.Name(), filepath.Ext(f.Name()))
+		fullName := f.Name()
+		extension := filepath.Ext(fullName)
+
+		fileName := strings.TrimSuffix(f.Name(), extension)
+		index := strings.Split(fileName, "-")[0]
+		title := strings.Split(fileName, "-")[1]
+
 		content, _ := os.ReadFile(filePath)
 
 		logItem := RawLogItem{
-			file:    fileName,
-			content: string(content),
+			index:      index,
+			title:      title,
+			fileName:   fileName,
+			rawContent: string(content),
 		}
 		result = append(result, logItem)
 	}
@@ -61,4 +75,34 @@ func renderMarkdown(content string) string {
 		map[djot_parser.DjotNode]djot_parser.Conversion{},
 	).ConvertDjotToHtml(&html_writer.HtmlWriter{}, ast[0])
 	return html
+}
+
+func prepareOutDir(dir string) {
+	fmt.Printf("Preparing %s dir...\n", dir)
+
+	// check if directory exists
+	if _, err := os.Stat(dir); os.IsNotExist(err) {
+		os.Mkdir(dir, os.ModePerm)
+		os.Create(filepath.Join(dir, FileGlowMark))
+	} else {
+		glowMark := filepath.Join(dir, FileGlowMark)
+		_, err := os.Stat(glowMark)
+		if os.IsNotExist(err) {
+			fmt.Printf("Out dir '%s' exists but doesn't have Glow mark", dir)
+			os.Exit(1)
+		}
+
+		files, _ := os.ReadDir(dir)
+		for _, f := range files {
+			if f.Name() == FileGlowMark {
+				continue
+			}
+			os.RemoveAll(filepath.Join(dir, f.Name()))
+		}
+	}
+}
+
+func buildHtmlFileContent(item RawLogItem) []byte {
+	htmlContent := renderMarkdown(item.rawContent)
+	return []byte(htmlContent)
 }
